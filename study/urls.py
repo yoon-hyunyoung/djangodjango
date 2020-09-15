b4361f40d9d56f9271d16e2bcc4c07ad0794a361
yoonhyunyoung <taupo28@gmail.com>
2020-09-15 10:45:37 +0900

    상세조회 수정 삭제 id에 int 속성 줌 (오타도 수정할 겸..)

--- study/urls.py
+++ study/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('students/', views.StudentView),
-    path('students/<id>', views.StudentDetailView),
+    path('students/<int:id>', views.StudentDetailView),
     path('scores/', views.ScoreView),
     
 ]
