84032994c4d2815aef611f30327064b44812a203
yoonhyunyoung <taupo28@gmail.com>
2020-09-15 10:38:18 +0900

    장고(api) 상사제회 수정 삭제 추가

--- study/urls.py
+++ study/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('students/', views.StudentView),
-    path('scores/', views.ScoreView)
+    path('students/<id>', views.StudentDetailView),
+    path('scores/', views.ScoreView),
     
 ]
--- study/views.py
+++ study/views.py
@@ -1,14 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework import status
 from rest_framework.decorators import api_view
 from .models import Students, Scores
 from .serializers import StudentSerializer, ScoreSerializer
 from rest_framework.response import Response
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def StudentView(request):
-    qs = Students.objects.all()
-    serializer = StudentSerializer(qs, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        qs = Students.objects.all()
+        serializer = StudentSerializer(qs, many=True)
+        return Response(serializer.data)
+    elif request.method =='POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def StudentDetailView(request, id):
+    qs = get_object_or_404(Students, pk=id)
+    #상세조회
+    if request.method == 'GET':
+        serializer = StudentSerializer(qs)
+        return Response(serializer.data)
+        #수정
+    elif request.method =="PUT":
+        serializer = StudentSerializer(qs, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+        #삭제
+    elif request.method == 'DELETE':
+        qs.delete()
+        return Response(status=404)
 
 @api_view(['GET'])
 def ScoreView(request):
