8fa5bfd11141b48ac97c768dd428882fdc5c819b
yoonhyunyoung <taupo28@gmail.com>
2020-09-17 18:37:13 +0900

    유저시리얼라이저 비밀ㅁ번호 숨기는 토큰 학습!

---
+++ account/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework_jwt.serializers import ModelSerializer
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model
+
+class SingupSerializer(ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email', 'phone_number']
+    
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            phone_number=validated_data['phone_number']
+        )  
+        #중요!!!
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
