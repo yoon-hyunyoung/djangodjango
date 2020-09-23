4b92194018714ed882728e48389869931b215aaa
yoonhyunyoung <taupo28@gmail.com>
2020-09-23 19:52:02 +0900

    시리얼라이즈로 다시 만드는 과정

--- yoonproject/admin.py
+++ yoonproject/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Todo
 
 # Register your models here.
+
+# Register your models here.``
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    # list_display = ['name', 'status', 'reg_date','end_date','del_yn','group']
+    pass
+시리얼 라이즈로 만들래요!!
